2f027c54ef3ba8f95c265e056f4d14c9c43a770d
OzneKx <kenzoalbuqk@gmail.com>
2025-10-04 00:17:44 -0300

    including user implementation

---
+++ src/main/java/com/application/bookstore/data/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.application.bookstore.data.repository;
+
+import com.application.bookstore.data.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+}
